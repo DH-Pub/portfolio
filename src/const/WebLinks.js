cd7ba5d288147ed872e252d94ea9ce24ca1f8175
DH-Pub <68387587+DH-Pub@users.noreply.github.com>
2023-10-25 01:01:32 +0700

    update link

--- src/const/WebLinks.js
+++ src/const/WebLinks.js
@@ -1,6 +1,6 @@
 export const WEBLINKS = {
   HOME: "/",
   GITHUB: "https://github.com/DH-Pub?tab=repositories",
-  LINKED_IN: "https://www.linkedin.com/in/h%C6%B0ng-%C4%91%C3%A0o-5298281aa/",
+  LINKED_IN: "https://www.linkedin.com/in/daobuiquanghung/",
   FACEBOOK: "https://www.facebook.com/hung.quang.56211497/",
 };

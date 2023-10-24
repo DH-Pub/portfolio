9f8df80e821233f582985c8ecbd19b32f2fecfb2
DH-Pub <68387587+DH-Pub@users.noreply.github.com>
2023-10-25 01:17:45 +0700

    router fix

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <HashRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter>
+  </React.StrictMode>
 );
